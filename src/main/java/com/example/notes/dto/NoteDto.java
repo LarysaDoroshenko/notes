bb7cafae53738675a15116ea0cab362106dae3a8
Larysa Doroshenko <larysad785@gmail.com>
2021-08-17 22:17:11 +0300

    feat: add endpoint to get all notes from memory

---
+++ src/main/java/com/example/notes/dto/NoteDto.java
@@ -0,0 +1,14 @@
+package com.example.notes.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class NoteDto {
+    
+    private String content;
+    
+}
